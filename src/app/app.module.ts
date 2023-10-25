a009ad22576c3ca703681ce7e47f3c7837d6e302
Juan Azocar <juazocar@profesor.duoc.cl>
2023-10-24 21:46:48 -0300

    mensaje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,23 @@ import { HttpClientModule} from '@angular/common/http';
 import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthenticationService } from './services/authentication.service';
-
+import { firebaseConfig } from '../environments/firebaseconfig';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [HttpClientModule, BrowserModule, 
+    IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    IonicStorageModule.forRoot()],
   providers: [SQLite, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AuthGuard,
     AuthenticationService, HttpClientModule],
   bootstrap: [AppComponent],
---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+     apiKey: "AIzaSyAdwKciysy2ieyeQh9yhTZ_SlBx49JVmgY",
+      authDomain: "backoffice-duoc.firebaseapp.com",
+      projectId: "backoffice-duoc",
+      storageBucket: "backoffice-duoc.appspot.com",
+      messagingSenderId: "776598132833",
+      appId: "1:776598132833:web:9fbb503b1fffd529023220",
+      measurementId: "G-M6W6ML8TTQ"
+  }
